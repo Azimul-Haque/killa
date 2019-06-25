70ffe5a03ac65191461ee06c0edd186c83938053
orbachinujbuk <orbachinujbuk@gmail.com>
2019-06-25 06:25:37 +0600

    June 25 Update!

--- resources/views/partials/_blog_sidebar.blade.php
+++ resources/views/partials/_blog_sidebar.blade.php
@@ -37,7 +37,7 @@
                 </a>
                 <div class="widget-posts-details">
                     <a href="{{ route('blog.single', $popular->slug) }}" class="overflowellipsis">{{ $popular->title }}</a>
-                    <span class="overflowellipsis">{{ $popular->user->name }} - {{ date('F d, Y', strtotime($popular->created_at)) }}</span>
+                    <span class="overflowellipsis">{{ $popular->user->name }} - {{ date('F d', strtotime($popular->created_at)) }}</span>
                 </div>
             </li>
             @endforeach
