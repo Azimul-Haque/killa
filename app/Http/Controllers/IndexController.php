4b20110d8ca120af28bd55a7eead842c1460619c
orbachinujbuk <orbachinujbuk@gmail.com>
2019-09-16 05:36:30 +0600

    :/

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -53,7 +53,7 @@ class IndexController extends Controller
 
     public function homeAdhoc()
     {
-        return route('index.index');
+        return redirect()->route('index.index');
     }
 
     public function getAbout()
