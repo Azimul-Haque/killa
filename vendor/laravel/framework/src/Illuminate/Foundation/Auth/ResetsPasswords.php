bd24668c52c5f35e957f939446fc67c677418025
orbachinujbuk <orbachinujbuk@gmail.com>
2019-09-16 05:27:49 +0600

    Updates

--- vendor/laravel/framework/src/Illuminate/Foundation/Auth/ResetsPasswords.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Auth/ResetsPasswords.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Password;
+use Session;
 
 trait ResetsPasswords
 {
@@ -311,6 +312,7 @@ trait ResetsPasswords
      */
     protected function getResetSuccessResponse($response)
     {
+        Session::flash('success', 'Password RESET success, logged in too!');
         return redirect($this->redirectPath())->with('status', trans($response));
     }
 
