5a0b5a276604c1e14dbbd4e02bfec9248957ba29
orbachinujbuk <orbachinujbuk@gmail.com>
2020-05-23 01:22:30 +0600

    ASD324

--- app/Http/Controllers/OneSignalController.php
+++ app/Http/Controllers/OneSignalController.php
@@ -188,7 +188,7 @@ class OneSignalController extends Controller
         if($api_key == 'rifat2020') {
             $questions = Charioteer::where('id', '>', $last_id)
                                    ->where('status', 1)
-                                   ->orderBy('id', 'desc')->get();
+                                   ->orderBy('id', 'asc')->get();
             print(json_encode($questions));
         }
     }
