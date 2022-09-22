3dffa971eac3bcbba9a03e5acff0a75a50e52456
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-23 00:15:31 +0600

    Rifat: Commit Fri 09/23/2022  0:15:31.03 608

--- app/Charioteer.php
+++ app/Charioteer.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Charioteer extends Model
 {
     public $timestamps = ['updated_at'];
+    const CREATED_AT = null; //and updated by default null set
 
     // public function setCreatedAtAttribute($value) { 
     //     $this->attributes['updated_at'] = \Carbon\Carbon::now(); 
